ab8e13d0fb1ea3dc9ef416a1675ed51fe31fbbf2
Hao Chen <hche8093@uni.sydney.edu.au>
2023-04-23 16:14:52 +1000

    Add components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import UserList from './components/UserList'
+
 function App() {
-	return <div>Hello World</div>
+	return (
+		<div className='container mx-auto'>
+			<UserList />
+		</div>
+	)
 }
 
 export default App
