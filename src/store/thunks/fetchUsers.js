1be1a833ae8e18f0a0df9fabe9ca20c8a5c1f713
Hao Chen <hche8093@uni.sydney.edu.au>
2023-04-23 17:30:57 +1000

    Add pause for testing

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -4,7 +4,16 @@ import axios from 'axios'
 const fetchUsers = createAsyncThunk('users/fetch', async () => {
 	const { data } = await axios.get('http://localhost:3005/users')
 
+	await pause(1000)
+
 	return data
 })
+
+// ! Dev only!!!
+const pause = (duration) => {
+	return new Promise((resolve) => {
+		setTimeout(resolve, duration)
+	})
+}
 
 export { fetchUsers }
