84f28d0e0852d169eedab824b92a88129be2be5e
Hao Chen <hche8093@uni.sydney.edu.au>
2023-04-25 12:12:30 +1000

    Add Album component

--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -3,7 +3,7 @@ import Button from './Button'
 import { removeUser } from '../store'
 import { useThunk } from '../hooks/use-thunk'
 import ExpandablePanel from './ExpandablePanel'
-import { Fragment } from 'react'
+import AlbumsList from './AlbumsList'
 
 function UsersListItem({ user }) {
 	const [doRemoveUser, isLoading, error] = useThunk(removeUser)
@@ -13,16 +13,20 @@ function UsersListItem({ user }) {
 	}
 
 	const header = (
-		<Fragment>
+		<>
 			<Button className='mr-3' loading={isLoading} onClick={handleClick}>
 				<GoTrashcan />
 			</Button>
 			{error && <div>Error deleting user...</div>}
 			{user.name}
-		</Fragment>
+		</>
 	)
 
-	return <ExpandablePanel header={header}>CONTENT!!</ExpandablePanel>
+	return (
+		<ExpandablePanel header={header}>
+			<AlbumsList user={user} />
+		</ExpandablePanel>
+	)
 }
 
 export default UsersListItem
