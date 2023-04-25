53606b311fb2b7de1a43fbbb9a14256de42a2c9e
Hao Chen <hche8093@uni.sydney.edu.au>
2023-04-25 11:02:17 +1000

    Add loading spinner button

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -43,11 +43,9 @@ function UserList() {
 		<div>
 			<div className='flex flex-row justify-between m-3'>
 				<h1 className='m-2 text-xl'>Users</h1>
-				{isCreatingUser ? (
-					'Creating user...'
-				) : (
-					<Button onClick={handleUserAdd}>+ Add User</Button>
-				)}
+				<Button loading={isCreatingUser} onClick={handleUserAdd}>
+					+ Add User
+				</Button>
 				{creatingUserError && 'Error creating user....'}
 			</div>
 			{renderedUsers}
