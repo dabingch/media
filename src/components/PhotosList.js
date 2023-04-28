53e493667f457fa9be501528bca64d5dbcdd30cd
Hao Chen <hche8093@uni.sydney.edu.au>
2023-04-28 11:25:44 +1000

    Add photo component

---
+++ src/components/PhotosList.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function PhotosList({ album }) {
+	return <div>PhotosList</div>
+}
+
+export default PhotosList
---
+++ src/components/PhotosListItem.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function PhotosListItem() {
+	return <div>PhotosListItem</div>
+}
+
+export default PhotosListItem
